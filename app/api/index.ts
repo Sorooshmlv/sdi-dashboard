95dfdf102b0fda3f3ecf359e4fb178d9708924d2
Soroosh Molavi <75088358+Sorooshmlv@users.noreply.github.com>
2025-05-17 19:29:33 +0330

    update 3.4

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,5 +1,5 @@
-import axios, { InternalAxiosRequestConfig, AxiosHeaders } from "axios";
-export const baseUrl = "http://193.36.85.71:8080/FROST-Server/v1.0";
+import axios from 'axios'
+export const baseUrl = 'http://193.36.85.71:8080/FROST-Server/v1.0'
 export const Api = axios.create({
-  baseURL: baseUrl,
-});
+  baseURL: baseUrl
+})
